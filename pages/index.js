9fa1a8a7b25e9f8d66799d76bdbe7c291440e3f9
Gustavochagas <gustavochagasb@gmail.com>
2021-01-25 20:40:35 -0300

    feat: added components

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import styled from 'styled-components'
-import db from '../db.json'
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`;
-
-const QuizContainer = styled.div`
+export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
@@ -21,10 +19,28 @@ const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-    <BackgroundImage>
+    <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-        asdasdas
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
       </QuizContainer>
-    </BackgroundImage>
-  )
+      <GitHubCorner projectUrl="https://github.com/gustavochagas" />
+    </QuizBackground>
+  );
 }
